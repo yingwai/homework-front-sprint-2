7a67e69f42a7bd59af3baed8fd8d9f421aee4686
yingwai <nick.yingwai@mail.ru>
2024-04-17 21:47:13 +0300

    HW4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,10 +18,10 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+        + (disabled ? ' ' + s.disabled : '')
+        + (!xType ? ' ' + s.default : '')
+        + (xType === 'red' ? ' ' + s.red : '')
+        + (xType === 'secondary' ? ' ' + s.secondary : '')
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
